f7bb8bbe13128d3480911a76ecd233452bda47e1
dongfu <1239927525@qq.com>
2017-07-11 09:08:32 +0800

    Add a file named README.MD and a function named partition2 to quick sort.

--- src/com/fuge/Sort.java
+++ src/com/fuge/Sort.java
@@ -9,8 +9,9 @@ public class Sort<T extends Comparable<T>> {
     }
 
     private void quickSort(T[] array, int low, int high) {
-        if (high > low) {
+        if (array != null && high > low ) {
             int mid = partition(array, low, high);
+//            int mid = partition2(array, low, high);
             quickSort(array, low, mid - 1);
             quickSort(array, mid + 1, high);
         }
@@ -31,5 +32,24 @@ public class Sort<T extends Comparable<T>> {
         }
         array[low] = pivot;
         return low;
+    }
+
+    private int partition2(T[] array, int low, int high) {
+        int small = low - 1;
+        for (int index = low; index < high; index++) {
+            if (array[index].compareTo(array[high]) < 0) {
+                small++;
+                if (array[index].compareTo(array[small]) < 0) {
+                    T temp = array[index];
+                    array[index] = array[small];
+                    array[small] = temp;
+                }
+            }
+        }
+        small++;
+        T temp = array[high];
+        array[high] = array[small];
+        array[small] = temp;
+        return small;
     }
 }
